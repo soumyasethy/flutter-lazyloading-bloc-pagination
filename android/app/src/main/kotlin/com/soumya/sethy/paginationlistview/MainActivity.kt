fea2927bff47324e5d4b5d485214e000139faf0e
Soumya Ranjan Sethy <soumyaranjan.sethy@bounceshare.com>
2020-06-21 16:34:13 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/soumya/sethy/paginationlistview/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.soumya.sethy.paginationlistview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'LazyLoadingWithBloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(),
+    );
+  }
+}

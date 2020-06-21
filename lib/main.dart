0faa8b35ade35a6ae4efe33c50c18e7b628b4188
Soumya Ranjan Sethy <soumyaranjan.sethy@bounceshare.com>
2020-06-21 16:35:30 +0530

    resolve dependency and used LazyLoading

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Container(),
+      home: LazyLoadingWithBloc(),
     );
   }
 }
